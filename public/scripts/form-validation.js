74dc8cc19029a46cb79924e763b10f3155d319ba
minhdao <minhdao6@gmail.com>
2018-05-10 17:35:43 +0700

    create public folder for static files

---
+++ public/scripts/form-validation.js
@@ -0,0 +1,24 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+// (function() {
+//   window.addEventListener('load', function() {
+//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//     var forms = document.getElementsByClassName('needs-validation');
+//     // Loop over them and prevent submission
+//     var validation = Array.prototype.filter.call(forms, function(form) {
+//       form.addEventListener('submit', function(event) {
+//         if (form.checkValidity() === false) {
+//           event.preventDefault();
+//           event.stopPropagation();
+//         }
+//         form.classList.add('was-validated');
+//       }, false);
+//     });
+//   }, false);
+// })();
+
+console.log('form validation loaded ... ');
+
+window.addEventListener('load', () => {
+    let forms = document.querySelectorAll('.needs-validaion');
+    console.log(forms);
+});
