cedd0d85139f5f5a4ef509c37542db4cd573a091
minhdao <minhdao6@gmail.com>
2018-05-10 12:52:32 +0700

    create mongoose and connect to mongodb - async/await used

---
+++ db/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+let dbConnect = async () => {
+    await mongoose.connect(process.env.MONGODB_UR);
+};
+
+dbConnect().then(() => {
+    console.log('Db Connection successful ... ');
+}).catch((error) => {
+    console.log('Db connection FAILED ...');
+    console.log(error);
+});
+
+module.exports = {
+    mongoose
+};
