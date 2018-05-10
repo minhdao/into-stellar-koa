084ffce11224b065c631e220d5c01a7ebc516dd3
minhdao <minhdao6@gmail.com>
2018-05-10 11:44:17 +0700

    create config files

---
+++ config/config.js
@@ -0,0 +1,14 @@
+/**
+ * Run configuration
+ * Need config.json to work
+ */
+
+let env = process.env.NODE_ENV || 'development';
+
+if (env === 'development' || env === 'test'){
+    let config = require('./config.json');
+    let envConfig = config[env];
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
+}
