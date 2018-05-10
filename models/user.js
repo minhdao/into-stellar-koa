436760bea98eed6301389a21310d5d8a44d5e38b
minhdao <minhdao6@gmail.com>
2018-05-10 15:24:26 +0700

    add in user model

---
+++ models/user.js
@@ -0,0 +1,103 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+var UserSchema = new mongoose.Schema({
+    giv_name: {
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true,
+        unique: false,
+    },
+    mid_name: {
+        type: String,
+        required: false,
+        minLength: 1,
+        trim: true,
+        unique: false,
+    },
+    fam_name: {
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true,
+        unique: false,
+    },
+    phone: {
+        type: Number,
+        required: true,
+        minLength: 1,
+        trim: true,
+        unique: false,
+    },
+    socialIdNum: {
+        type: String,
+        required: false,
+        minLength: 1,
+        trim: true,
+        unique: true,
+    },
+    bankName: {
+        type: String,
+        required: false,
+        minLength: 1,
+        trim: true,
+        unique: false,
+    },
+    bankAccountNum: {
+        type: String,
+        required: false,
+        minLength: 1,
+        trim: true,
+        unique: false,
+    },
+    bankAccountName: {
+        type: String,
+        required: false,
+        minLength: 1,
+        trim: true,
+        unique: false,
+    },
+    bankCity: {
+        type: String,
+        required: false,
+        minLength: 1,
+        trim: true,
+        unique: false,
+    },
+    email: {
+        type: String,
+        required: true,
+        minLength: 1,
+        trim: true,
+        unique: true,
+        validate:{
+            validator: (value) => {
+                return validator.isEmail(value);
+            },
+            message: '{VALUE} is not an email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    activated: {
+        type: Boolean,
+        default: false,
+        required: true
+    },
+    tokens: [
+        {
+            access:{
+                type: String,
+                required: true
+            },
+            token: {
+                type: String,
+                required: true
+            }
+        }
+    ]
+});
