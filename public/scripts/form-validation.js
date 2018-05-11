57a2148958e17bfd401af2008a86494cc2b5e02a
minhdao <minhdao6@gmail.com>
2018-05-11 10:36:20 +0700

    attempt front end validataion failed - will try backend :D

--- public/scripts/form-validation.js
+++ public/scripts/form-validation.js
@@ -1,5 +1,20 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
+// console.log('Load form-validation.js ...');
+// window.addEventListener('load', () => {
+//     let forms = document.querySelectorAll('.needs-validation');
+//     console.log(forms);
+//     var validation = Array.prototype.filter.call(forms, function(form) {
+//       form.addEventListener('submit', function(event) {
+//         if (form.checkValidity() === false) {
+//           event.preventDefault();
+//           event.stopPropagation();
+//         }
+//         form.classList.add('was-validated');
+//       }, false);
+//     });
+// });
 // (function() {
+//   'use strict';
 //   window.addEventListener('load', function() {
 //     // Fetch all the forms we want to apply custom Bootstrap validation styles to
 //     var forms = document.getElementsByClassName('needs-validation');
@@ -15,10 +30,3 @@
 //     });
 //   }, false);
 // })();
-
-console.log('form validation loaded ... ');
-
-window.addEventListener('load', () => {
-    let forms = document.querySelectorAll('.needs-validaion');
-    console.log(forms);
-});
