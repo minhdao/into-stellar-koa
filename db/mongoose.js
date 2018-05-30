d0be29b04c8c6738e8a0738f0befba81f7731f09
minhdao <minhdao6@gmail.com>
2018-05-30 17:22:25 +0700

    clean up code

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,16 +3,14 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 let dbConnect = async () => {
-    await mongoose.connect(process.env.MONGODB_URI);
+  await mongoose.connect(process.env.MONGODB_URI);
 };
 
-dbConnect().then(() => {
-    console.log('Db Connection successful ... ');
-}).catch((error) => {
-    console.log('Db connection FAILED ...');
-    console.log(error);
+dbConnect().then(() => {}).catch((error) => {
+  console.log('Db connection FAILED ...');
+  console.log(error);
 });
 
 module.exports = {
-    mongoose
+  mongoose
 };
