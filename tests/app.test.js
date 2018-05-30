b1a330fbec4d5e1bb5b7ec8a58d97255ade88d94
minhdao <minhdao6@gmail.com>
2018-05-30 17:23:39 +0700

    checking test for correct inputs

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,7 @@
 const expect = require('expect');
 const supertest = require('supertest');
 const app = require('./../app.js').app;
-const { invalidUserObjects } = require('./seeds/app.seed.js');
+const { validUserObjects, invalidUserObjects } = require('./seeds/app.seed.js');
 
 let request = supertest(app);
 
@@ -15,40 +15,64 @@ describe('app.js GET /register', () => {
 });
 
 describe('app.js POST /register', () => {
-  it('should reject 400 request without any of required names', (done) => {
-
+  it('should be 200 for request with all correct inputs', (done) => {
+    request
+      .post('/register')
+      .send(validUserObjects.user_1)
+      .expect(200)
+      .end(done);
   });
 
-  it('should reject 400 request without a phone number', (done) => {
+  // it('should reject 400 request without any of required names', (done) => {
+  //   request
+  //     .post('/register')
+  //     .send(invalidUserObjects.missingRequiredName)
+  //     .expect(400)
+  //     .end(done);
+  // });
+  //
+  // it('should reject 400 request without a phone number', (done) => {
+  //   request
+  //     .post('/register')
+  //     .send(invalidUserObjects.missingPhone)
+  //     .expect(400)
+  //     .end(done);
+  // });
+  //
+  // it('should reject 400 request with wrong phone number format', (done) => {
+  //   request
+  //     .post('/register')
+  //     .send(invalidUserObjects.incorrectPhoneFormat)
+  //     .expect(400)
+  //     .end(done);
+  // });
+  //
+  // it('should reject 400 request without email', (done) => {
+  //   request
+  //     .post('/register')
+  //     .send(invalidUserObjects.missingEmail)
+  //     .expect(400)
+  //     .end(done);
+  // });
+  //
+  // it('should reject 400 request for email with wrong format', (done) => {
+  //   request
+  //     .post('/register')
+  //     .send(invalidUserObjects.invalidEmail)
+  //     .expect(400)
+  //     .end(done);
+  // });
 
-  });
-
-  it('should reject 400 request with wrong phone number format', (done) => {
-
-  });
-
-  it('should reject 400 request with wrong phone number format', (done) => {
-
-  });
-
-  it('should reject 400 request without email', (done) => {
-
-  });
-
-  it('should reject 400 request for email with wrong format', (done) => {
-
-  });
-
-  it('should reject 400 request for email already exist in DB', (done) => {
-
-  });
-
-  it('should reject 400 request without any of passwords', (done) => {
-
-  });
-
-  it('should reject 400 request with passwords not matching', (done) => {
-
-  });
+  // it('should reject 400 request for email already exist in DB', (done) => {
+  //
+  // });
+  //
+  // it('should reject 400 request without any of passwords', (done) => {
+  //
+  // });
+  //
+  // it('should reject 400 request with passwords not matching', (done) => {
+  //
+  // });
 
 });
