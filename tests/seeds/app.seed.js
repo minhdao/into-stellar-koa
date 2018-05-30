5d8177781bc075bc2133b446d5a8ac76e48bcfd9
minhdao <minhdao6@gmail.com>
2018-05-30 17:22:46 +0700

    more seed data for test cases

--- tests/seeds/app.seed.js
+++ tests/seeds/app.seed.js
@@ -1,5 +1,23 @@
 const { ObjectID } = require('mongodb');
 
+let validUserObjects = {
+  user_1: {
+    fam_name: 'minh',
+    mid_name: '',
+    giv_name: 'dao',
+    phone: '0123456789',
+    socialIdNum: '',
+    email: 'test1@gmail.com',
+    password: [
+      'daotienminh',
+      'daotienminh'
+    ],
+    bankAccountNum: '',
+    bankAccountName: '',
+    bankCity: ''
+  },
+};
+
 let invalidUserObjects = {
   missingRequiredName: {
     fam_name: '',
@@ -15,9 +33,85 @@ let invalidUserObjects = {
     bankAccountNum: '',
     bankAccountName: '',
     bankCity: ''
+  },
+  missingPhone: {
+    fam_name: 'minh',
+    mid_name: '',
+    giv_name: 'dao',
+    phone: '',
+    socialIdNum: '',
+    email: 'test1@gmail.com',
+    password: [
+      'daotienminh',
+      'daotienminh'
+    ],
+    bankAccountNum: '',
+    bankAccountName: '',
+    bankCity: ''
+  },
+  incorrectPhoneFormat: {
+    fam_name: 'minh',
+    mid_name: '',
+    giv_name: 'dao',
+    phone: '121bcb456',
+    socialIdNum: '',
+    email: 'test1@gmail.com',
+    password: [
+      'daotienminh',
+      'daotienminh'
+    ],
+    bankAccountNum: '',
+    bankAccountName: '',
+    bankCity: ''
+  },
+  missingEmail: {
+    fam_name: 'minh',
+    mid_name: '',
+    giv_name: 'dao',
+    phone: '123456789',
+    socialIdNum: '',
+    email: '',
+    password: [
+      'daotienminh',
+      'daotienminh'
+    ],
+    bankAccountNum: '',
+    bankAccountName: '',
+    bankCity: ''
+  },
+  invalidEmail: {
+    fam_name: 'minh',
+    mid_name: '',
+    giv_name: 'dao',
+    phone: '123456789',
+    socialIdNum: '',
+    email: 'minhemail.com',
+    password: [
+      'daotienminh',
+      'daotienminh'
+    ],
+    bankAccountNum: '',
+    bankAccountName: '',
+    bankCity: ''
+  },
+  missingPasswords: {
+    fam_name: 'minh',
+    mid_name: '',
+    giv_name: 'dao',
+    phone: '',
+    socialIdNum: '',
+    email: 'test1@gmail.com',
+    password: [
+      '',
+      ''
+    ],
+    bankAccountNum: '',
+    bankAccountName: '',
+    bankCity: ''
   }
 };
 
 module.exports = {
+  validUserObjects,
   invalidUserObjects
 };
