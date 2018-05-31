b9e982b3d9f3ec276c65d7d4275e3e34631284b5
minhdao <minhdao6@gmail.com>
2018-05-31 13:40:54 +0700

    create custom validation error class

---
+++ errors/ValidationError.js
@@ -0,0 +1,17 @@
+class ValidationError extends Error {
+  constructor(message, validInputs, inValidInputs) {
+    super(message);
+    this._validInputs = inValidInputs;
+    this._inValidInputs = inValidInputs;
+  }
+  get validInputs() {
+    return this._validInputs;
+  }
+  get inValidInputs() {
+    return this._inValidInputs;
+  }
+}
+
+module.exports = {
+  ValidationError
+};
